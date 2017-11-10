463707f4f1d406dd42904a74ac7885ca1519aad3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-10 15:32:17 -0500

    Set up characters and get sword to move

--- js/scene2.js
+++ js/scene2.js
@@ -7,48 +7,60 @@ function scene2() {
   stageImg.setAttribute('src', castleImgSrc);
 
   // Setup Cast
-  var nedContainer = document.createElement('div')
-  var ned = document.querySelector('#ned');
-  var leather = document.querySelector('#leather-armour');
+  var nedContainer = document.createElement('div');
+  stage.append(nedContainer);
   nedContainer.style.position = 'absolute';
   nedContainer.style.width = '10%;';
   nedContainer.style.height = '37%';
-  nedContainer.style.top = '55%';
+  nedContainer.style.top = '50%';
   nedContainer.style.bottom = '50%';
   nedContainer.style.left = '25%';
-  ned.style.position = 'absolute';
-  ned.style.top = '0';
-  ned.style.width = '70%';
-  leather.style.position = 'absolute';
-  leather.style.bottom = '0';
+
+  var ned = document.querySelector('#ned');
   nedContainer.appendChild(ned);
+  ned.style.position = 'absolute';
+  ned.style.top = 0;
+  ned.style.width = '70%';
+
+  var leather = document.querySelector('#leather-armour');
   nedContainer.appendChild(leather);
-  stage.append(nedContainer);
+  leather.style.position = 'absolute';
+  leather.style.bottom = 0;
 
-  var joffContainer = document.createElement('div');
-  var joffrey = document.querySelector('#joffrey');
-  var king = document.querySelector('#king');
-  joffContainer.style.position = "absolute";
-  offContainer.style.bottom = '0';
-  joffrey.style.position = 'absolute';
-  joffrey.style.top = '0';
-  joffrey.style.right = '0';
-  joffrey.style.width = '58%';
-  king.style.position = 'absolute'
-  king.style.bottom: '0';
-  joffContainer.appendChild(joffrey);
-  joffContainer.appendChild(king);
-  stage.append(joffContainer);
+  var joffContainer = document.createElement('div')
+  joffContainer.id = 'joff-container'
+  stage.append(joffContainer)
+  joffContainer.style.width = '8%'
+  joffContainer.style.position = 'absolute'
+  joffContainer.style.height = '31%'
+  joffContainer.style.top = '55%'
+  joffContainer.style.left = '55%'
 
+  var joff = document.querySelector('#joffrey')
+  joffContainer.append(joff)
+  joff.style.width = '75%'
+  joff.style.position = 'absolute'
+  joff.style.left = '33%'
 
+  var kingWardrobe = document.querySelector('#king')
+  joffContainer.append(kingWardrobe)
+  kingWardrobe.style.position = 'absolute'
+  kingWardrobe.style.bottom = 0
 
   // Character 1
 
   // Character 2
 
   // Setup Props
+  var sword = document.querySelector('#sword')
+  stage.append(sword)
 
   // Prop 1
+  sword.style.position = 'absolute'
+  sword.style.top = '78%'
+  sword.style.left = '26%'
+  sword.style.height = '5%'
+  sword.style.transform = 'rotateY(150deg)'
 
   // Prop 2
 
@@ -57,13 +69,26 @@ function scene2() {
   // Stage direction 1
 
   frame(function() {
-
+    sword.style.top = '78%'
+    sword.style.left = '26%'
   })
-
-  // Stage direction 2
 
   frame(function() {
-
+    sword.style.top = '78%'
+    sword.style.left = '26%'
   })
+
+  // new middle stages
+  frame(function() {
+    sword.style.top = '72%'
+    sword.style.left = '39%'
+  })
+
+  frame(function() {
+    sword.style.top = '64%'
+    sword.style.left = '52%'
+  })
+  // Stage direction 2
+
 
 }
